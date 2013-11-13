0644d4637308492e806a3f4524ac521d85ac2675
Gerald Schneider <gerald@schneidr.de>
2013-11-13 07:39:00 +0000

    init

---
+++ yaogp.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Yet another Open Graph Plugin
+Plugin URI: http://scm.schneidr.de/yaogp
+Description: This plugin adds Open Graph meta tags to your WordPress site
+Version: 0.1
+Author: Gerald Schneider
+Author URI: http://schneidr.de/
+*/
+
+
+?>
